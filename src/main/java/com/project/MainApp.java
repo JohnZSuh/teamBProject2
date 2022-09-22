2714354c4f0aa1203af8ff11030d1fe2c21d2ac2
NickNSx <nickogir@gmail.com>
2022-09-22 15:51:49 -0500

    Add spring startup and dependency

--- src/main/java/com/project/MainApp.java
+++ src/main/java/com/project/MainApp.java
@@ -1,41 +1,20 @@
 package com.project;
 
-import org.apache.catalina.LifecycleException;
-import org.apache.catalina.startup.Tomcat;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
-import org.springframework.web.servlet.DispatcherServlet;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
-import com.project.config.AppConfig;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
+@SpringBootApplication
 public class MainApp {
-    
-    private static Logger logger = LogManager.getLogger(MainApp.class);
-    public static void main(String[] args) throws LifecycleException {
+    public static void main(String[] args) {
         
-        logger.info("Starting Reimbursement app");
-
-        String docBase = System.getProperty("java.io.tmpdir");
-
-        Tomcat webServer = new Tomcat();
-        webServer.setBaseDir(System.getProperty(docBase));
-        webServer.setPort(5000);
-        webServer.getConnector(); //* server to receive request
-
-        AnnotationConfigWebApplicationContext beanContainer = new AnnotationConfigWebApplicationContext();
-        beanContainer.register(AppConfig.class);
-
-        //* Web server context and servlet config
-        final String rootContext = "/Reimbursement";
-        webServer.addContext(rootContext, docBase);
-        webServer.addServlet(rootContext, "DispatcherServlet", new DispatcherServlet(beanContainer)).addMapping("/");
-
-        //* starting and awaiting web request
-        webServer.start();
-
-        logger.info("Reimbursement started");
-
-        webServer.getServer().await();
+        SpringApplication.run(MainApp.class, args);
+    }
+    
+    @Bean
+    public ObjectMapper jsonMapper() {
+        return new ObjectMapper();
     }
 }
