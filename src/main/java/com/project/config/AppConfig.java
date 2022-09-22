5d1577c787570f60aeefb166137a6c9b72ff6b04
NickNSx <nickogir@gmail.com>
2022-09-21 20:49:06 -0500

    Add AppConfig class

---
+++ src/main/java/com/project/config/AppConfig.java
@@ -0,0 +1,22 @@
+package com.Nick.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan("com.Nick")
+@PropertySource("classpath:application.properties")
+public class AppConfig {
+
+    @Bean
+    public ObjectMapper jsonMapper() {
+        return new ObjectMapper();
+    }
+    
+}
