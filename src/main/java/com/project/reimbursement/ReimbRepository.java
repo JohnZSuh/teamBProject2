dee15d3dbf4daa16eff60d9b9a2de04453e0bbdb
JohnZSuh <john.z.suh@gmail.com>
2022-09-24 18:54:26 -0500

    Created a reimbursement response class and empty reimbursement repository

---
+++ src/main/java/com/project/reimbursement/ReimbRepository.java
@@ -0,0 +1,12 @@
+package com.project.reimbursement;
+
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ReimbRepository extends JpaRepository<Reimbursement, UUID> {
+
+    
+}
