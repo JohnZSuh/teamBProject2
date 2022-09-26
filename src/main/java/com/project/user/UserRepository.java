640a8734dacca23940823f5e471ed8e3040beb76
NickNSx <nickogir@gmail.com>
2022-09-26 13:37:03 -0500

    removed unused imports

--- src/main/java/com/project/user/UserRepository.java
+++ src/main/java/com/project/user/UserRepository.java
@@ -1,12 +1,7 @@
 package com.project.user;
 
-import com.project.auth.Credentials;
-import com.project.common.exceptions.AuthenticationException;
-import com.project.common.exceptions.InvalidRequestException;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import org.springframework.stereotype.Service;
 
 import java.util.Optional;
 import java.util.UUID;
@@ -18,6 +13,6 @@ public interface UserRepository extends JpaRepository<User, UUID> {
     Optional<User> findUserByUsername(String username);
     Optional<User> findUserByEmail(String email);
 
-    //    @Query(nativeQuery = true, value = "SELECT * FROM app_users WHERE username = :1 AND password :2")
+    // @Query(nativeQuery = true, value = "SELECT * FROM app_users WHERE username = :1 AND password :2")
     Optional<User> findUserByUsernameAndPassword(String username, String password);
 }
