cb2ad0170916225bbd245542e60592226ce84d63
NickNSx <nickogir@gmail.com>
2022-09-27 23:23:09 -0500

    Added update request status in repository

--- src/main/java/com/project/reimbursement/ReimbRepository.java
+++ src/main/java/com/project/reimbursement/ReimbRepository.java
@@ -3,11 +3,16 @@ package com.project.reimbursement;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ReimbRepository extends JpaRepository<Reimbursement, UUID> {
 
     // save to database
-    String save(NewRequest newRequest);
+    void save(NewRequest newRequest);
+
+    @Query(nativeQuery = true, value = "UPDATE ers_reimbursements SET resolver_id = :aId, resolved = current_date, status_id = :sId WHERE reimb_id = :rId")
+    void updateStatus(@Param("aId") String resolverId, @Param("sId") String statusId, @Param("rId") String reimbId);
 }
